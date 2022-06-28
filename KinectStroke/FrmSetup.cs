b841dbcf8a303d7c8b32748fd21aa184d6633776
Whisky-Jack <joshtindall1@gmail.com>
2022-06-28 11:50:23 -0700

    Added simple form

---
+++ KinectStroke/FrmSetup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MeepEngine
+{
+    public partial class FrmSetup : Form
+    {
+        public FrmSetup()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmSetup_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
